571fc6fb10e15c4dc1dee2bfd0085961c133808f
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-04-12 15:30:33 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -33,27 +33,27 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "⚘—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—⚘————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——⚘———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———⚘——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————⚘—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————⚘————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————⚘———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————⚘——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————⚘—"
     else:
-        bar = "—————————◉"
+        bar = "—————————⚘"
     buttons = [
-                [
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
@@ -62,28 +62,30 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-                 [
+        [
             InlineKeyboardButton(
-
-                text="ᴅᴇᴠ 💕",
-
-                url=f"t.me/{OWNER_USERNAME}",
-
+                text="✪ʟɪꜰᴇ✪",
+                url=f"https://t.me/angel_world11",
             ),
             InlineKeyboardButton(
-
-                text="💌 ɢʀᴏᴜᴘ",
-
-                url=f"{SUPPORT_CHAT}",
-
+                text="✪ᴊᴀɴɴ✪",
+                url=f"https://t.me/mr_editx",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✪ᴏᴡɴᴇʀ✪",
+                url=f"t.me/{OWNER_USERNAME}",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
+        
 
 
 def stream_markup(_, chat_id):
@@ -91,28 +93,40 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
                  [
             InlineKeyboardButton(
 
-                text="ᴅᴇᴠ 💕",
+                text="✪ʟɪꜰᴇ✪",
 
-                url=f"t.me/{OWNER_USERNAME}",
+                url=f"https://t.me/angel_world11",
 
             ),
             InlineKeyboardButton(
 
-                text="💌 ɢʀᴏᴜᴘ",
+                text="✪ᴊᴀɴɴ✪",
 
-                url=f"{SUPPORT_CHAT}",
+                url=f"https://t.me/mr_editx",
+
+            ),
+        ],
+
+                 [
+            InlineKeyboardButton(
+
+                text="✪ᴏᴡɴᴇʀ✪",
+
+                url=f"t.me/{OWNER_USERNAME}",
 
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
+    
 
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
@@ -184,3 +198,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
+    
